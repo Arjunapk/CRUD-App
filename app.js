e3bbff1df2dc33c17c38461bffc0a9e9d3636e26
Arjunapk <arjun030401@gmail.com>
2023-06-23 13:13:48 +0530

    initial code

---
+++ app.js
@@ -0,0 +1,57 @@
+const { response } = require('express')
+const express = require('express')
+const { request } = require('http')
+const path = require('path')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+
+const app = express()
+app.use(express.json())
+
+const dbPath = path.join(__dirname, 'crudDatabase.db')
+let db = null
+
+const initializeDBAndServer = async () => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        })
+        app.listen(3000, () => {
+            console.log("Server Running at http://localhost:3000/")
+        })
+    } catch (error) {
+        console.log(`DB Error: ${error}`)
+        process.exit(1)
+    }
+}
+
+initializeDBAndServer()
+
+app.get('/crud/user-details/', async (request, response) => {
+    const getUserDetailsQuery = 'SELECT * FROM user_details;'
+    const data = await db.all(getUserDetailsQuery)
+    response.send(data)
+})
+
+app.post('/crud/create-user/', async (request, response) => {
+    const {id, name, gender, email, mobile_number, dob} = request.body
+    const getCreateUserDetailsQuery = `INSERT INTO user_details (id, name, gender, email, mobile_number, dob) VALUES (${id}, '${name}', '${gender}', '${email}', ${mobile_number}, '${dob}');`
+    await db.run(getCreateUserDetailsQuery)
+    response.send('User Created Successfully')
+})
+
+app.post('/crud/update-user/:userId/', async (request, response) => {
+    const {userId} = request.params
+    const {name, gender, email, mobile_number, dob} = request.body
+    const getUpdateUserDetailsQuery = `UPDATE user_details SET name = '${name}', gender = '${gender}', email = '${email}', mobile_number = '${mobile_number}', dob = '${dob}' WHERE id = ${userId};`
+    await db.run(getUpdateUserDetailsQuery)
+    response.send("User Details Updated Successfully")
+})
+
+app.delete('/crud/delete-user/:userId/', async (request, response) => {
+    const {userId} = request.params
+    const getDeleteUserQuery = `DELETE FROM user_details WHERE id = ${userId};`
+    await db.run(getDeleteUserQuery)
+    response.send('User Details Deleted Successfully')
+})
